701329c7adacb54f1d4c78cd6a9e9dfca87296b6
Molnár Attila <molnar.attila@szlgbp.hu>
2025-09-15 13:23:47 +0200

    láncolt lista

--- LancoltLista/Program.cs
+++ LancoltLista/Program.cs
@@ -1,16 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace LancoltLista
 {
 	internal class Program
 	{
-
-
-
-		
 
 		class LancoltLista<T>
 		{
@@ -56,16 +54,80 @@ namespace LancoltLista
 			}
 
 			private Elem<T> fejelem;
-
+			private int count;
+			public int Count { get => count; }
 			public LancoltLista()
 			{
 				this.fejelem = new Elem<T> ();
+				this.count = 0;
 			}
 
 			public void Add(T ujertek)
 			{
 				new Elem<T>(this.fejelem.bal, ujertek);
+				count++;
 			}
+
+
+			private Elem<T> Keres(T ertek)
+			{
+				Elem<T> aktelem = fejelem.jobb;
+				while (aktelem != fejelem && !aktelem.value.Equals(ertek)) 
+				{
+					aktelem = aktelem.jobb;
+				}
+				return aktelem;
+			}
+
+			public void Remove(T ertek)
+			{
+				Elem<T> elem = Keres(ertek);
+				if (elem == fejelem)
+				{
+					Console.Error.WriteLine($"Hát igazából nem volt \"{ertek}\" elem, úgyhogy nem töröltem semmit.");
+				}
+				else
+				{
+					elem.Töröl();
+					count--;
+				}
+			}
+
+			public void RemoveAll(T ertek)
+			{
+				Elem<T> aktelem = fejelem.jobb;
+				while (aktelem != fejelem)
+				{
+					aktelem = aktelem.jobb;
+					if (aktelem.bal.value.Equals(ertek))
+					{
+						aktelem.bal.Töröl();
+						count--;
+					}
+				}
+			}
+
+			public void RemoveAt(int i)
+			{
+				if (count <= i)
+				{
+					throw new ArgumentOutOfRangeException($"Túl nagy a(z) {i}, mert ez a lista csak {count} elemből áll!");
+				}
+
+				if (i < 0)
+				{
+					throw new ArgumentOutOfRangeException($"Negatív a(z) {i}!");
+				}
+
+				Elem<T> aktelem = fejelem.jobb;
+				for (int l = 0; l < i; l++)
+				{
+					aktelem=aktelem.jobb;
+				}
+				aktelem.Töröl();
+				count--;
+			}
+
 
 			public override string ToString()
 			{
@@ -88,14 +150,57 @@ namespace LancoltLista
 			Console.WriteLine(lista);
 			lista.Add(4);
 			Console.WriteLine(lista);
+			lista.Add(0);
+			Console.WriteLine(lista);
 			lista.Add(2);
+			Console.WriteLine(lista);
+			lista.Add(0);
 			Console.WriteLine(lista);
 			lista.Add(9);
 			Console.WriteLine(lista);
 			lista.Add(0);
 			Console.WriteLine(lista);
+			lista.Add(0);
+			Console.WriteLine(lista);
 			lista.Add(12);
 			Console.WriteLine(lista);
+
+			lista.Remove(9);
+			Console.WriteLine(lista);
+			lista.Remove(42);
+			Console.WriteLine(lista);
+			lista.Add(0);
+			Console.WriteLine(lista);
+			lista.RemoveAll(43);
+			Console.WriteLine(lista);
+			lista.RemoveAll(0);
+			Console.WriteLine(lista);
+
+			Console.WriteLine("törlések:0. és 2. elemek:");
+			lista.RemoveAt(0);
+			Console.WriteLine(lista);
+			lista.RemoveAt(2);
+			Console.WriteLine(lista);
+			lista.RemoveAt(2);
+			Console.WriteLine(lista);
+
+
+
+			List<string> hagyomanyos_lista = new List<string>();
+			hagyomanyos_lista.Remove("alma");
+			hagyomanyos_lista.RemoveAt(0);
+			Console.WriteLine(hagyomanyos_lista.Count);
+			hagyomanyos_lista.Contains("körte");
+
+
+			hagyomanyos_lista.FindIndex(x => x.Length > 3);
+			hagyomanyos_lista.FindLastIndex(x => x.Length > 3);
+			hagyomanyos_lista.First(x => x.Length > 3);
+			hagyomanyos_lista.Last(x => x.Length > 3);
+			hagyomanyos_lista.Where(x => x.Length > 3);
+			hagyomanyos_lista.Select(x => x.Length);
+
+
 
 
 		}
