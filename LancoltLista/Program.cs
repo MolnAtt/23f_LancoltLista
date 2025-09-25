07c4e0ee3751e35f01e7b05ac9f38e872b6a176e
Molnár Attila <molnar.attila@szlgbp.hu>
2025-09-25 09:10:21 +0200

    linq függvények és reverse

--- LancoltLista/Program.cs
+++ LancoltLista/Program.cs
@@ -55,7 +55,7 @@ namespace LancoltLista
 
 			private Elem<T> fejelem;
 			private int count;
-			public int Count { get => count; }
+			public int Length { get => count; }
 			public LancoltLista()
 			{
 				this.fejelem = new Elem<T> ();
@@ -130,7 +130,7 @@ namespace LancoltLista
 			/// </summary>
 			/// <param name="predikatum">A tulajdonság</param>
 			/// <returns>A megtalált első elem indexe.</returns>
-			public int FindIndex() => Count == 0 ? -1 : 0;
+			public int FindIndex() => Length == 0 ? -1 : 0;
 			public int FindIndex(Func<T, bool> predikatum)
 			{
 				int i = 0;
@@ -143,14 +143,130 @@ namespace LancoltLista
 				return aktelem == fejelem ? -1 : i;
 			}
 
-			// FindLastIndex
-			// Where
-			// Count
-			// Select
-			// First
-			// Last
-			// Sum
+
+			public int FindLastIndex(Func<T, bool> predikatum)
+			{
+				int i = Length-1;
+				Elem<T> aktelem = fejelem.bal;
+				while (aktelem != fejelem && !predikatum(aktelem.value))
+				{
+					i--;
+					aktelem = aktelem.bal;
+				}
+				return i;
+			}
+
+			public LancoltLista<T> Where(Func<T, bool> predikatum)
+			{
+				LancoltLista<T> result = new LancoltLista<T>();
+				Elem<T> aktelem = fejelem.jobb;
+				while (aktelem != fejelem)
+				{
+					if (predikatum(aktelem.value))
+					{
+						result.Add(aktelem.value);
+					}
+					aktelem = aktelem.jobb;
+				}
+				return result;
+			}
+
+			public int Count() => count;
+			public int Count(Func<T, bool> predikatum)
+			{
+				int db = 0;
+				Elem<T> aktelem = fejelem.jobb;
+				while (aktelem != fejelem)
+				{
+					if (predikatum(aktelem.value))
+					{
+						db++;
+					}
+					aktelem = aktelem.jobb;
+				}
+				return db;
+			}
+
+			public LancoltLista<S> Select<S>(Func<T, S> selector)
+			{
+				LancoltLista<S> result = new LancoltLista<S>();
+				Elem<T> aktelem = fejelem.jobb;
+				while (aktelem != fejelem)
+				{
+					result.Add(selector(aktelem.value));
+					aktelem = aktelem.jobb;
+				}
+				return result;
+			}
+
+			public T First() => fejelem.jobb.value;
+			public T First(Func<T, bool> predikatum)
+			{
+				Elem<T> aktelem = fejelem.jobb;
+				while (aktelem != fejelem && !predikatum(aktelem.value))
+				{
+					aktelem = aktelem.jobb;
+				}
+				if (fejelem == aktelem)
+				{
+					throw new Exception("Ilyen tulajdonságú elem nem volt a listában!");
+				}
+				return aktelem.value;
+			}
+			public T Last() => fejelem.bal.value;
+			public T Last(Func<T, bool> predikatum)
+			{
+				Elem<T> aktelem = fejelem.bal;
+				while (aktelem != fejelem && !predikatum(aktelem.value))
+				{
+					aktelem = aktelem.bal;
+				}
+				if (fejelem == aktelem)
+				{
+					throw new Exception("Ilyen tulajdonságú elem nem volt a listában!");
+				}
+				return aktelem.value;
+			}
+
+			public double Sum(Func<T, double> selector)
+			{
+				double sum = 0;
+				Elem<T> aktelem = fejelem.jobb;
+				while (aktelem != fejelem)
+				{
+					sum += selector(aktelem.value);
+					aktelem = aktelem.jobb;
+				}
+				return sum;
+			}
+
 			// Reverse
+
+			public void Reverse()
+			{
+				Elem<T> j = fejelem.jobb;
+				Elem<T> b = fejelem.bal;
+				if (Length % 2 == 0)
+				{
+					while (j.bal != b)
+					{
+						(j.value, b.value) = (b.value, j.value);
+						j = j.jobb;
+						b = b.bal;
+					}
+				}
+				else
+				{
+					while (j != b)
+					{
+						(j.value, b.value) = (b.value, j.value);
+						j = j.jobb;
+						b = b.bal;
+					}
+				}
+
+
+			}
 
 
 			public override string ToString()
@@ -188,6 +304,15 @@ namespace LancoltLista
 			Console.WriteLine(lista);
 			lista.Add(12);
 			Console.WriteLine(lista);
+			lista.Add(9);
+			Console.WriteLine(lista);
+			lista.Add(10);
+			Console.WriteLine(lista);
+			lista.Add(11);
+			Console.WriteLine(lista);
+			lista.Add(12);
+			Console.WriteLine(lista);
+
 
 			lista.Remove(9);
 			Console.WriteLine(lista);
@@ -195,36 +320,36 @@ namespace LancoltLista
 			Console.WriteLine(lista);
 			lista.Add(0);
 			Console.WriteLine(lista);
-			lista.RemoveAll(43);
+			//lista.RemoveAll(43);
+			//Console.WriteLine(lista);
+			//lista.RemoveAll(0);
+			//Console.WriteLine(lista);
+
+			//Console.WriteLine("törlések:0. és 2. elemek:");
+			//lista.RemoveAt(0);
+			//Console.WriteLine(lista);
+			//lista.RemoveAt(2);
+			//Console.WriteLine(lista);
+
+			Console.WriteLine("ez most a fordított lista:");
+			lista.Reverse();
 			Console.WriteLine(lista);
-			lista.RemoveAll(0);
-			Console.WriteLine(lista);
-
-			Console.WriteLine("törlések:0. és 2. elemek:");
-			lista.RemoveAt(0);
-			Console.WriteLine(lista);
-			lista.RemoveAt(2);
-			Console.WriteLine(lista);
-			lista.RemoveAt(2);
-			Console.WriteLine(lista);
 
 
 
-			List<string> hagyomanyos_lista = new List<string>();
-			hagyomanyos_lista.Remove("alma");
-			hagyomanyos_lista.RemoveAt(0);
-			Console.WriteLine(hagyomanyos_lista.Count);
-			hagyomanyos_lista.Contains("körte");
+			//List<string> hagyomanyos_lista = new List<string>();
+			//hagyomanyos_lista.Remove("alma");
+			//hagyomanyos_lista.RemoveAt(0);
+			//Console.WriteLine(hagyomanyos_lista.Count);
+			//hagyomanyos_lista.Contains("körte");
 
 
-			hagyomanyos_lista.FindIndex(x => x.Length > 3);
-			hagyomanyos_lista.FindLastIndex(x => x.Length > 3);
-			hagyomanyos_lista.First(x => x.Length > 3);
-			hagyomanyos_lista.Last(x => x.Length > 3);
-			hagyomanyos_lista.Where(x => x.Length > 3);
-			hagyomanyos_lista.Select(x => x.Length);
-
-
+			//hagyomanyos_lista.FindIndex(x => x.Length > 3);
+			//hagyomanyos_lista.FindLastIndex(x => x.Length > 3);
+			//hagyomanyos_lista.First(x => x.Length > 3);
+			//hagyomanyos_lista.Last(x => x.Length > 3);
+			//hagyomanyos_lista.Where(x => x.Length > 3);
+			//hagyomanyos_lista.Select(x => x.Length);
 
 
 		}
